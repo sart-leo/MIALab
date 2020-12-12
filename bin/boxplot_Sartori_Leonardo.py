5b99274a721a9549a8705b5b599bf08fd815300c
ls15r086 <ls15r086@campus.unibe.ch>
2020-12-12 13:03:42 +0100

    MODIFY box plot to decide what to plot by commenting in/out plotDice, plotHdrf and/or plotHistogram

--- bin/boxplot_Sartori_Leonardo.py
+++ bin/boxplot_Sartori_Leonardo.py
@@ -5,10 +5,17 @@ import os
 
 
 def main():
+    folder = "../mia-results-final/"
+    data_dice, data_hdr, labels, dirs = loadData(folder)
+    plotDice(data_dice, labels, dirs)
+    plotHdrf(data_hdr, labels, dirs)
+    #plotHistograms(data_dice, labels, dirs)
 
-    dirs = [dI for dI in os.listdir("../mia-results-final/") if os.path.isdir(os.path.join("../mia-results-final/", dI))]
 
-    test_el = pd.read_csv("../mia-results-final/" + dirs[0] + "/results.csv", sep=';')
+def loadData(folder):
+    dirs = [dI for dI in os.listdir(folder) if os.path.isdir(os.path.join(folder, dI))]
+
+    test_el = pd.read_csv(folder + dirs[0] + "/results.csv", sep=';')
     labels_t = set(test_el['LABEL'])
     print(test_el)
     data_dice = np.zeros((len(dirs), int(len(test_el) / len(labels_t)), len(labels_t)))
@@ -23,6 +30,10 @@ def main():
             data_dice[h, :, i] = results.loc[results['LABEL'] == label]['DICE']
             data_hdr[h, :, i] = results.loc[results['LABEL'] == label]['HDRFDST']
 
+    return data_dice, data_hdr, labels, dirs
+
+
+def plotDice(data_dice, labels, dirs):
     for i, label in enumerate(labels):
         fig = plt.figure(figsize=(7, 7))
         ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
@@ -31,7 +42,10 @@ def main():
         plt.ylabel('DSC')
         ax.set_xticklabels(dirs, rotation=90)
         ax.set_ylim(0, 1)
+    plt.show()
 
+
+def plotHdrf(data_hdr, labels, dirs):
     for i, label in enumerate(labels):
         fig = plt.figure(figsize=(7,7))
         ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
@@ -40,16 +54,18 @@ def main():
         plt.ylabel('HDRFDST')
         ax.set_xticklabels(dirs, rotation=90)
         ax.set_ylim(0, np.max(data_hdr))
-        #fig.subplots_adjust(wspace=1)
+    plt.show()
 
+
+def plotHistograms(data, labels, dirs):
     for i, label in enumerate(labels):
         for h, d in enumerate(dirs):
             fig = plt.figure(figsize=(7, 7))
             ax = fig.add_axes([0.1, 0.3, 0.8, 0.6])
             plt.title(label)
-            ax.hist(data_hdr[h, :, i].T, bins=20)
-
+            ax.hist(data[h, :, i].T, bins=20)
     plt.show()
+
 
 
 if __name__ == '__main__':
